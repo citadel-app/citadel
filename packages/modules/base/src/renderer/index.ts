d1596d0cae37a5684f68c92f23da332f83c7d8cd
Nilay Vishwakarma <iwannabebot@gmail.com>
2026-03-27 04:55:50 +0530

    bump version to 1.0.2

--- packages/modules/base/src/renderer/index.ts
+++ packages/modules/base/src/renderer/index.ts
@@ -4,7 +4,7 @@ import { BaseAppHost } from './BaseAppHost';
 
 export const BaseModule: IModule = {
     id: '@citadel-app/base',
-    version: '1.0.1',
+    version: '1.0.2',
     globalComponents: [
         { region: 'app-host', component: BaseAppHost }
     ],
